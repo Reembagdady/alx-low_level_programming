f82f36ebe97429efad9b429e006e4ea5912a731e
Reembagdady <reemonda2001@gmail.com>
2023-10-24 12:27:58 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,12 +3,17 @@
  * main - Entry point
  *
  * Return: always 0
-*/
+ */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
